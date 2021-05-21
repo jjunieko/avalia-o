6de3a8c3d8526f3c10701fb3b7f8e369ab3617a8
jjunieko <juniorgtr123@gmail.com>
2021-05-21 18:10:00 -0300

    teste para avaliação

---
+++ react-frontend/src/components/HeaderComponent.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+class HeaderComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <header>
+                    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <div><a href="#" className="navbar-brand">App de gerenciamento de funcionário</a></div>
+                    </nav>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default HeaderComponent
